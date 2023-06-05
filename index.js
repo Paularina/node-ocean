684d434866b0414c230d418a9e8933d4536d0f13
Paula Rocha <paula.r.rocha@hotmail.com>
2023-06-05 12:08:16 -0300

    Start project

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const express = require('express')
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.get("/oi", function (req, res) {
+  res.send("Hi World");
+});
+
+
+app.listen(3000,function(){
+  console.log('aplicacao funcionando e rodando na porta 3000')
+});

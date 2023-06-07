a9ae5f07516177a2afc327cc798621b74934f025
Paula Rocha <paula.r.rocha@hotmail.com>
2023-06-07 12:01:02 -0300

    CRUD com Express

--- index.js
+++ index.js
@@ -1,15 +1,58 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.send("Hello World 2");
 });
 
 app.get("/oi", function (req, res) {
   res.send("Hi World");
 });
 
+const herois = ["Mulher Maravilha", "Capitã Marvel", "Homen de Ferro"];
 
-app.listen(3000,function(){
+app.get("/herois", function(req, res) {
+  res.send(herois.filter(Boolean));
+});
+
+app.post("/herois", function(req, res) {
+  
+  const nome = req.body.nome;
+  
+  herois.push(nome);
+
+  res.send("Criado com sucesso!");
+});
+
+app.get("/herois/:id", function(req, res){
+  const id = req.params.id;
+  
+  const item = herois[id - 1];
+  
+  res.send(item);
+});
+
+app.put("/herois/:id", function(req, res){
+  const id = req.params.id;
+
+  const novoNome = req.body.nome;
+
+  herois[id - 1] = novoNome;
+
+  res.send("Atualizado com sucesso!");
+});
+
+app.delete("/herois/:id", function(req, res){
+  const id = req.params.id;
+  
+  delete herois[id - 1];
+  
+  res.send("Deletada");
+});
+
+app.listen(3000, function(){
   console.log('aplicacao funcionando e rodando na porta 3000')
 });
+
